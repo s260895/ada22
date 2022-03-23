e6a7151b43ddd629db3dd695bb159c5818dfe4e2
s260895 <s260895@gmail.com>
2022-03-23 22:11:13 +0100

    upd

--- ruleevaluator-ms/main.py
+++ ruleevaluator-ms/main.py
@@ -2,7 +2,7 @@ from asyncio.windows_events import NULL
 from datetime import datetime
 import pandas
 import ccxt
-import pyti
+from pyti.exponential_moving_average import exponential_moving_average
 import pickle
 import time
 from datetime import datetime
@@ -28,19 +28,19 @@ def rule_evaluator(event,context):
         "id": 1,
         "name": "Bitcoin",
         "ticker": "BTCUSDT",
-        "prices": [42553.70,42553.70,42553.70,42553.70]
+        "prices": [1,10,100,10]
         },
         {
         "id": 2,
         "name": "Ethereum",
         "ticker": "ETHUSDT",
-        "prices": [42553.70,42553.70,42553.70,42553.70]
+        "prices": [1000,100,10,1]
         },
         {
         "id": 3,
         "name": "Binance",
         "ticker": "BNBUSDT",
-        "prices": [42553.70,42553.70,42553.70,42553.70]
+        "prices": [1,10,1,10]
         }
     ]
 
@@ -69,7 +69,7 @@ def rule_evaluator(event,context):
             "user_id": 1,
             "date_opened": 1648058104,
             "date_closed": None,
-            "open_price": None,
+            "open_price": "1200",
             "close_price": None
         }
     ]
@@ -85,7 +85,7 @@ def rule_evaluator(event,context):
 
     strategy_params = {
         'strat_name':'ema_cross_over_under',
-        'technical_indicators': {'fast_ema':4,'slow_ema':20}
+        'technical_indicators': {'fast_ema':2,'slow_ema':3}
     }
 
     prices_per_stock = 4
@@ -101,22 +101,29 @@ def rule_evaluator(event,context):
         closes = [float(price) for price in stock_db['prices']]
 
         '''Calculate "Aggregate" Indicator from all Technical Indicators'''
-        indicator = pandas.DataFrame(pyti.ema(closes,strategy_params['technical_indicators']['fast_ema'])
-                                - pyti.ema(closes,strategy_params['slow_ema']),
+        indicator = pandas.DataFrame(exponential_moving_average(closes,strategy_params['technical_indicators']['fast_ema'])
+                                - exponential_moving_average(closes,strategy_params['technical_indicators']['slow_ema']),
                                 columns=['ema_diff'])
         '''Reduce Length of All Columns'''
-        closes = closes[(prices_per_stock+1):]
-        indicator = indicator[(prices_per_stock+1):]
+        # closes = closes[(prices_per_stock+1):]
+        # indicator = indicator[(prices_per_stock+1):]
         '''Compute Signal Column (permissible values 1,-1,0)'''
-        signal = [0] + [1 if float(indicator.loc[index]) > 0 and float(indicator.loc[index-1]) < 0
-                        else -1 if float(indicator.loc[index]) < 0 and float(indicator.loc[index-1]) > 0  
-                        else 0 for index in indicator.index[1:]]
+        signal = [1 if  float(closes[index])>float(closes[index-1])
+                    else -1 if float(closes[index])<float(closes[index-1])
+                    else 0
+                    for index in range(len(closes))]
 
+        # [0] + [1 if float(indicator.loc[index]) > 0 and float(indicator.loc[index-1]) < 0
+        #                 else -1 if float(indicator.loc[index]) < 0 and float(indicator.loc[index-1]) > 0  
+        #                 else 0 for index in indicator.index[1:]]
+
+        # print(signal)
+        print(stock_db['ticker'])
         '''
             if signal not 0, fetch all users 
             GET /user_stocks (argument: stock id)
         '''
-        if signal[-1] is not 0:
+        if signal[-1] != 0:
             '''
                 If Non Zero Signal
                 Make Transaction with 
@@ -143,14 +150,24 @@ def rule_evaluator(event,context):
                         "user_id": user_id,
                         "date_opened": datetime.now(),
                         "date_closed": None,
-                        "open_price": stock_db['price'],
+                        "open_price": stock_db['prices'],
                         "close_price": None
                     }
+
+                    user_stocks_db.append(new_user_stock)
                     # POST /user_stocks, body = new_user_stock
 
             if signal[-1] == -1:
-                user_ids = set([user_stock_db['user_id'] for user_stock_db in user_stocks_db if (user_stock_db['stock_id'] == stock_db['id']) and (user_stock_db['date_closed'] == None)])
+                
+                selected_user_stocks = [user_stock_db for user_stock_db in user_stocks_db if (user_stock_db['stock_id'] == stock_db['id']) and (user_stock_db['date_closed'] == None)]
+                user_ids = [selected_user_stock['user_id'] for selected_user_stock in selected_user_stocks]
+                
+                
+                # print(user_stocks_db)
+                # print(user_ids)
                 for user_id in user_ids:
+                    # print(user_stocks_db)
+                    print("123")
                     # 1. SELL transaction to be forwarded to the transaction-ms
                     new_transaction = {
                         "symbol": stock_db['ticker'],
@@ -163,15 +180,19 @@ def rule_evaluator(event,context):
                     # POST /transactions, body = new_transaction
 
                     # 2. Close an UserStock in userstock-ms
-                    selected_user_stock = [user_stock_db for user_stock_db in user_stocks_db if user_stock_db['user_id'] == user_id][0]
-
+                    selected_user_stock = [selected_user_stock for selected_user_stock in selected_user_stocks if selected_user_stock['user_id'] == user_id][0]
+                    print(selected_user_stock)
+                    # print(selected_)
                     selected_index = user_stocks_db.index(selected_user_stock)
                     
                     selected_user_stock['date_closed'] = datetime.now()
-                    selected_user_stock['close_price'] = 'Fetched by price fetcher?'
+                    selected_user_stock['close_price'] = stock_db['prices'][-1]
+
 
 
                     user_stocks_db[selected_index] = selected_user_stock
+
+                    print(user_stocks_db)
 
 
                     # new_user_stock = {
@@ -184,4 +205,3 @@ def rule_evaluator(event,context):
                     # }
 
     return user_stocks_db
-    
