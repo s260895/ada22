7750f21c654feb965e31bcde395c3f93c6a1a454
romendb <romendb@gmail.com>
2022-05-17 21:44:48 +0200

    data json

--- transaction-ms/broker.py
+++ transaction-ms/broker.py
@@ -1,4 +1,5 @@
 import os
+import json
 from google.cloud import pubsub_v1
 from random import randint
 
@@ -9,21 +10,16 @@ publisher = pubsub_v1.PublisherClient()
 def create_transaction(transaction_type, stock_id, user_id):
     """Fuction to forward the transaction to the broker"""
     # create dummy data
-    price = randint(0, 10)
-    data = "New Transaction".encode("utf-8")
-    # publish the message and print to console
-    message = publisher.publish(
-        os.environ["GOOGLE_TOPIC_PATH"],
-        data,
-        transaction_type=transaction_type,
-        stock_id=str(stock_id),
-        user_id=str(user_id),
-        price=str(price))
-    print("Transaction forwarded: {}".format(message.result()))
-    # return the transaction data
-    return {
+    data = {
         "transaction_type": transaction_type,
         "stock_id": stock_id,
         "user_id": user_id,
-        "price": price,
+        "price": randint(0, 10),
     }
+
+    data = json.dumps(data).encode("utf-8")
+    # publish the message and print to console
+    message = publisher.publish(os.environ["GOOGLE_TOPIC_PATH"], data)
+    print("Transaction forwarded: {}".format(message.result()))
+    # return the transaction data
+    return data
