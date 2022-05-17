736bff31cdc28f7decb5a53a3e302b09db8adcb2
romendb <romendb@gmail.com>
2022-05-17 14:26:03 +0200

    content type error fix

--- price-fetch-ms/main.py
+++ price-fetch-ms/main.py
@@ -11,7 +11,7 @@ def price_fetcher(request):
         The response text or any set of values that can be turned into a Response object using `make_response <http://flask.pocoo.org/docs/1.0/api/#flask.Flask.make_response>`.
     """
     content_type = request.headers["content-type"]
-    if content_type == "application/json":
+    if content_type == "application/json; charset=UTF-8":
         stock_list = request.get_json(silent=True)
         if not stock_list:
             raise ValueError("JSON is invalid.")
